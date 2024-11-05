269ff4866e1b23dc148ada72dffbb9d84698b53a
Muhammad Rehan Alam <rehan@beebolt.com>
2024-11-05 13:52:05 +0500

    Chore (Vite): vite react project setup

---
+++ vite.config.ts
@@ -0,0 +1,46 @@
+import { sentryVitePlugin } from '@sentry/vite-plugin';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import Checker from 'vite-plugin-checker';
+import svgr from 'vite-plugin-svgr';
+
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/',
+
+  plugins: [
+    react(),
+    svgr(),
+    sentryVitePlugin({
+      org: 'beebolt',
+      project: 'bb-freight-web',
+      reactComponentAnnotation: { enabled: true },
+    }),
+    Checker({ typescript: true })
+  ],
+
+  // for dev
+  server: {
+    port: 4000,
+    strictPort: true,
+    // TODO: Causing issue while loading assets using alias
+    // origin: 'http://0.0.0.0:4000',
+  },
+
+  //change port for production
+  preview: {
+    port: 4001,
+    strictPort: true,
+  },
+
+  //paths for typescript
+  resolve: {
+    
+  },
+
+  build: {
+    sourcemap: true,
+  },
+});
